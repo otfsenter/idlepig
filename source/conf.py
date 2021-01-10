d5203a6615184f0593b056f94f66c6f6a7d642ee
otfsenter <newellzhou@163.com>
2021-01-11 00:26:34 +0800

    add english method in english directory

--- source/conf.py
+++ source/conf.py
@@ -36,7 +36,7 @@ extensions = [
 ]
 
 
-# source_suffix = ['.rst', '.ipynb']
+source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
