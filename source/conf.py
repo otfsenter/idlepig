c679c5c945c276fd0f16d4021a8c89aa1f297cf3
otfsenter <newellzhou@163.com>
2021-02-01 23:46:43 +0800

    add index in md, delete md suffix in conf

--- source/conf.py
+++ source/conf.py
@@ -35,7 +35,8 @@ extensions = [
 ]
 
 
-source_suffix = ['.rst', '.md']
+# source_suffix = ['.rst', '.md']
+source_suffix = ['.rst']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
